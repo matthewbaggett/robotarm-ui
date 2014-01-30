f52a9ebe1ae7e939a0fe60131c2ca22958c64d00
Matthew Baggett <matthew@baggett.me>
2014-01-30 05:14:43 +0000

    Layout

--- htdocs/index.php
+++ htdocs/index.php
@@ -15,7 +15,7 @@ foreach($lsusb_out as $lsusb_out_row){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dozer C&C</title>
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
 
@@ -32,43 +32,26 @@ foreach($lsusb_out as $lsusb_out_row){
 
   </head>
   <body>
-    <div class="navbar-wrapper">
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
-
-        <div role="navigation" class="navbar navbar-inverse navbar-static-top">
-          <div class="container">
-            <div class="navbar-header">
-              <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a href="#" class="navbar-brand">Dozer C&amp;C</a>
-            </div>
-            <div class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                  <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Dozer C&amp;C</a>
         </div>
-
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
     </div>
+
     <div data-ride="carousel" class="carousel slide" id="myCarousel">
       <div class="carousel-inner">
         <div class="item active">
