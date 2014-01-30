16415139b1244e4b6cf78fecb31355abd2f4e4c6
Matthew Baggett <matthew@baggett.me>
2014-01-30 03:36:46 +0000

    Second rewrite.

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,17 +8,8 @@
       var websocket = 'ws://<?php echo $_SERVER['HTTP_HOST']; ?>:8084/';
     </script>
     <script type="text/javascript" src="js/jquery.js"></script>
-    <script type="text/javascript" src="js/jsmpg.js"></script>
-    <script type="text/javascript" src="js/canvas.js"></script>
   </head>
   <body>
-    <canvas id="videoCanvas" width="640" height="480">
-      <p>
-        Please use a browser that supports the Canvas Element, like
-        <a href="http://www.google.com/chrome">Chrome</a>,
-        <a href="http://www.mozilla.com/firefox/">Firefox</a>,
-        <a href="http://www.apple.com/safari/">Safari</a> or Internet Explorer 10
-      </p>
-    </canvas>
+    <img src="<?php echo $_SERVER['HTTP_HOST']; ?>:8080/?action=stream" />
   </body>
 </html>
