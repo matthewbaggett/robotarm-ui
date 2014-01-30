1a8296f4885644f09624fc7f2c93d86175f66235
Matthew Baggett <matthew@baggett.me>
2014-01-30 04:09:08 +0000

    Adding Bootstrap

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,19 +1,91 @@
 <?php
-$host = explode(":", $_SERVER['HTTP_HOST'],2);
+$host = explode(":", $_SERVER['HTTP_HOST'], 2);
 $host = reset($host);
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="viewport" content="width=320, initial-scale=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dozer C&C</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
-    <script type="text/javascript">
-      var websocket = 'ws://<?php echo $_SERVER['HTTP_HOST']; ?>:8084/';
-    </script>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="js/html5shiv.js"></script>
+    <script type="text/javascript" src="js/respond.min.js"></script>
+    <![endif]-->
     <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/buttons.js"></script>
   </head>
   <body>
-    <img src="http://<?php echo $host ?>:8080/?action=stream" />
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <div role="navigation" class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a href="#" class="navbar-brand">Project name</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                  <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div data-ride="carousel" class="carousel slide" id="myCarousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li class="active" data-slide-to="0" data-target="#myCarousel"></li>
+        <li data-slide-to="1" data-target="#myCarousel" class=""></li>
+        <li data-slide-to="2" data-target="#myCarousel" class=""></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="http://<?php echo $host ?>:8080/?action=stream" />
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>DOZER DERP</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; <?php echo date("Y"); ?> Matthew Baggett.</p>
+      </footer>
+
+    </div>
+
   </body>
 </html>
