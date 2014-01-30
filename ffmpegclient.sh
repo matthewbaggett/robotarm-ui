5f97b5a6bf8de3a1afdcafa032ce8408f9cea8b9
Matthew Baggett <matthew@baggett.me>
2014-01-30 12:37:33 +0000

    Not using ffmpeg any more

--- ffmpegclient.sh
+++
@@ -1,2 +0,0 @@
-sudo killall -9 ffmpeg;
-sudo ffmpeg -v verbose -r 5 -s 320x160 -f video4linux2 -i /dev/video0 http://milan:9999/dozer.ffm
--- ffmpegserver.sh
+++
@@ -1,2 +0,0 @@
-sudo killall -9 ffserver;
-sudo ffserver -f ffserver.conf;
